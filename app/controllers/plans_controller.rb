848f9854f3869af1d40e72629cebc757663a78c8
Doug Alcorn <dougalcorn@gmail.com>
2009-10-14 22:24:15 -0400

    some simple UI for creating a plan

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,18 @@
 class PlansController < ApplicationController
+  resource_controller
+  before_filter :load_buys
+
+  private
+
+  def collection
+    @collection ||= Plan.all
+  end
+
+  def build_object
+    @object ||= Plan.new(params[:plan])
+  end
+
+  def load_buys
+    @buys = Buy.all
+  end
 end
--- test/integration/plans_controller_test.rb
+++ test/integration/plans_controller_test.rb
@@ -1,10 +1,47 @@
 require 'test_helper'
 
 class PlansControllerTest < ActionController::IntegrationTest
-  fixtures :all
+  context "new plan" do
+    setup do
+      @buy = Factory(:buy)
+      @buy2 = Factory(:buy)
+      visit new_plan_path
+    end
+    context "creating with valid data" do
+      setup do
+        fill_in "advertiser name", :with => "Tighty Whities"
+        select @buy.site_name, :from => "buys"
+        click_button "save"
+      end
+      should "not have any errors" do
+        assert_not_contain "error"
+      end
+      should_change "Plan.count", :by => 1
+    end
 
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+    # webrat doesn't handle multi-selects well and I didn't want to muck with it
+    # context "with multiple buys" do
+    #   setup do
+    #     fill_in "advertiser name", :with => "Tighty Whities"
+    #     select @buy.site_name, :from => "buys"
+    #     select @buy2.site_name, :from => "buys"
+    #     click_button "save"
+    #   end
+    #   should_change "Plan.count", :by => 1
+    #   should_assign_to :plan
+    #   should "have multiple buys for the plan" do
+    #     assert_equal 2, assigns(:plan).buys.size
+    #   end
+    # end
+
+    context 'creating without a buy' do
+      setup do
+        fill_in "advertiser name", :with => "Tighty Whities"
+        click_button "save"
+      end
+      should "contain errors" do
+        assert_contain "error"
+      end
+    end
   end
 end
